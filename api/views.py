6493aa3f8f8035bf068d1b53302e7d621bdb2af2
martin-martin <breuss.martin@gmail.com>
2020-04-16 17:24:04 +0300

    Create viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from blog.models import Post
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows posts to be viewed or edited."""
+    queryset = Post.objects.all().order_by('-published')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
